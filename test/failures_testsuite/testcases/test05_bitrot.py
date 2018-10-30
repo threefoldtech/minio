d40c47888494ef099ab350799ea6c7b78af10bd8
john-kheir <kheirj@codescalers.com>
2018-10-30 12:59:16 +0200

    adapting bitrot testcase

--- test/failures_testsuite/testcases/test05_bitrot.py
+++ test/failures_testsuite/testcases/test05_bitrot.py
@@ -1,11 +1,13 @@
-from random import randint
 from base_test import BaseTest
-import unittest, time
+import unittest
+from utils.failures import robot_god_token
+from jumpscale import j
+
 
 class TestS3Failures(BaseTest):
     def setUp(self):
         super().setUp()
-        
+
     def tearDown(self):
         super().tearDown()
 
@@ -13,53 +15,82 @@ class TestS3Failures(BaseTest):
     def test001_bitrot(self):
         """
         - Get the minio namespaces and get the zdbs location.
-        - Upload a file and get its md5 sum, should succeed.
-        - Manipulate some data of the uploaded file in any zdb location that has data due to upload.
+        - Make sure all zdbs are up.
+        - Get the extra 25% namespaces down.
+        - Upload a file (F1) and get its md5sum, should succeed.
+        - Corrupt all data in any zdb location.
+        - Get parity zdbs down excluding the corrupted namespace.
+        - Try to download F1, should give wrong md5.
+        - Get parity zdbs up exculding extra_namespaces zdbs.
         - Run the bitrot protection, should succeed.
+        - Get parity zdbs down again excluding corrupted namespace zdb.
         - Check that the corrupted data has been corrected, should succeed.
-        - Download the file, Must have same md5 as the uploaded file.
+        - Download F1 again, should have same md5 as the uploaded file.
+        - Make sure all zdbs are up.
         """
         self.logger.info('Get the minio namespaces and get the zdbs location')
         ser = self.s3.dm_robot.services.names[self.s3_service_name]
         namespaces = ser.data['data']['namespaces']
+        parityshards = self.s3.service.data['data']['parityShards']
+        datashards = self.s3.service.data['data']['dataShards']
 
-        self.logger.info('Upload a file and get its md5 sum, should succeed.')
-        self.logger.info('Create a file and check its md5sum.')
-        self.file_name = self.upload_file()
-        md5_before = self.file_name
+        #make this in the setup if possible
+        self.logger.info('Make sure all zdbs are up')
+        self.s3.failures.zdb_up(count=len(namespaces))
 
-        self.logger.info('Manipulate some data of the uploaded file in any zdb location that has data due to upload')
-        for namespace in namespaces:
-            robot = j.clients.zrobot.robots[namespace['node']]
-            ns = robot.services.get(name=namespace['name'])
-            zdb = robot.services.get(name=ns.data['data']['zerodb'])
-            zdb_path = zdb.data['data']['path']
-            # connect to the namespace node
-            node_url = namespaces[0]['url'].split(':6600')[0].split('http://')[1]
-            node = j.clients.zos.get('john', data={'host': node_url})
-            # check the files there in zdb path
-            ns_name = ns.data['data']['nsName']
-            data = node.client.bash('ls {}/data/{}'.format(zdb_path, ns_name)).get().stdout
-            files1 = data.split()
-            if files1:
-                break
+        self.logger.info('Get the extra 25% namespaces down')
+        extra_namespaces_num = len(namespaces) - parityshards - datashards
+        extra_namespaces = [ns['name'] for ns in namespaces[:extra_namespaces_num - 1]]
+        self.s3.failures.zdb_down(count=extra_namespaces_num, except_namespaces=namespaces[extra_namespaces_num:])
 
-        # check the added files and manpiulate them
-        zdb_file = '{}/data/{}/{}'.format(zdb_path, ns_name, files1[0])
-        zdb_file_md5_before = node.client.bash('md5sum {}'.format(zdb_file)).get().stdout.split()[0]
-        node.client.bash('dd conv=notrunc if=/dev/urandom of={} bs=1M count=1'.format(zdb_file)).get()
+        self.logger.info('Upload a file and get its md5sum, should succeed.')
+        file_name, bucket_name, md5_before = self.s3.upload_file()
+
+        self.logger.info('Corrupt all data in any zdb location')
+        # can make a function to corrupt shards in given namespace
+        namespace = namespaces[extra_namespaces_num:][0]
+        robot = j.clients.zrobot.robots[namespace['node']]
+        robot = robot_god_token(robot)
+        ns = robot.services.get(name=namespace['name'])
+        zdb = robot.services.get(name=ns.data['data']['zerodb'])
+        zdb_path = zdb.data['data']['path']
+        # connect to the namespace node
+        node_url = namespace['url'].split(':6600')[0].split('http://')[1]
+        node = j.clients.zos.get('namespace_node', data={'host': node_url})
+        # check the files there in zdb path
+        ns_name = ns.data['data']['nsName']
+        data = node.client.bash('ls {}/data/{}'.format(zdb_path, ns_name)).get().stdout
+        files = data.split()
+        self.assertTrue(files, "No shards were found in this namespace")
+        for f in files:
+            zdb_file = '{}/data/{}/{}'.format(zdb_path, ns_name, f)
+            node.client.bash('dd if=/dev/urandom of={} bs=1M count=10'.format(zdb_file)).get()
+
+        self.logger.info('Get parity zdbs down excluding the corrupted namespace')
+        wrong_data_namespace = namespace['name']
+        self.s3.failures.zdb_down(count=parityshards, except_namespaces=wrong_data_namespace)
+
+        self.logger.info('Try to download F1, should give wrong md5')
+        md5_after = self.s3.download(file_name, bucket_name)
+        self.assertNotEqual(md5_before, md5_after, "md5 after download is not different")
+
+        self.logger.info('Get parity zdbs up exculding extra_namespaces zdbs')
+        self.s3.failures.zdb_up(parityshards, except_namespaces=extra_namespaces)
 
         # run the bitrot protections and then wait
         self.logger.info('Run the bitrot protection, should succeed')
+        result = self.s3.minio_container.client.system('minio gateway zerostor-repair --config-dir /bin').get()
 
-        self.logger.info('Check that the corrupted data has been corrected, should succeed')
-        zdb_file_md5_after = node.client.bash('md5sum {}'.format(zdb_file)).get().stdout.split()[0]
-        self.assertEqual(zdb_file_md5_before, zdb_file_md5_after)
+        self.logger.info('Get parity zdbs down again excluding corrupted namespace zdb')
+        self.s3.failures.zdb_down(count=parityshards, except_namespaces=wrong_data_namespace)
 
         # download  and and assert
-        self.logger.info('Download the file, Must have same md5 as the uploaded file')
+        self.logger.info('Download F1 again , should have same md5 as the uploaded file')
         md5_after = self.download_file(file_name=self.file_name)
         self.assertEqual(md5_after, md5_before)
+
+        self.logger.info('Make sure all zdbs are up')
+        self.s3.failures.zdb_up(count=len(namespaces))
 
     def test002_kill_minio_process(self):
         """
