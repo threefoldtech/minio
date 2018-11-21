1245453ab20333137fc534dfad89154260c3f145
0xIslamTaha <islamtaha2012@gmail.com>
2018-11-21 18:44:43 +0200

    add logs

--- test/failures_testsuite/testcases/s3/test01_tlog.py
+++ test/failures_testsuite/testcases/s3/test01_tlog.py
@@ -48,23 +48,23 @@ class TestTlog(BaseTest):
         """
         self.s3.failures.tlog_down()
         time.sleep(60)
-        self.logger.info('Upload file, should fail')
+        self.logger.info('upload file, should fail')
         with self.assertRaises(RuntimeError):
             self.s3.upload_file()
 
         self.s3.failures.tlog_up()
         for _ in range(10):
-            self.logger.info('wait till tlog  be up')
             if self.s3.failures.tlog_status():
                 break
             else:
+                self.logger.info('wait till tlog  be up')
                 time.sleep(60)
         else:
             self.assertTrue(self.s3.failures.tlog_status())
 
-        self.logger.info('Upload file, should success')
+        self.logger.info('upload file, should success')
         file_name, bucket_name, md5_before = self.s3.upload_file()
-        self.logger.info('Download file, should success')
+        self.logger.info('download file, should success')
         md5_after = self.s3.download_file(file_name, bucket_name)
         self.assertEqual(md5_after, md5_before)
 
