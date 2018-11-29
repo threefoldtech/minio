ce20e8970a2fb6129d0356a3ad92f5ecceadba3b
0xIslamTaha <islamtaha2012@gmail.com>
2018-11-29 16:57:02 +0200

    update zdb test cases

--- test/failures_testsuite/testcases/s3/test02_zdb.py
+++ test/failures_testsuite/testcases/s3/test02_zdb.py
@@ -24,45 +24,34 @@ class ZDBFailures(BaseTest):
         """
 
         test002_upload_stop_parity_zdb_download
-        - upload 1M, should succeed.
+        - upload 2M, should succeed.
         - Download file, should succeed
-        - Deleted the downloaded file
         - assert md5 checksum is matching
-        - Stop n zdb, n <= parity
-        - Download file, should succeed
+        - Stop n zdb, n = parity namespaces
+        - Upload a file, should fail
+        - Download the old file, should succeed
         - assert md5 checksum is matching
         - Start n zdb
         """
+        self.logger.info('upload file')
         file_name, bucket_name, md5_before = self.s3.upload_file()
+
+        self.logger.info('download file')
         md5_after = self.s3.download_file(file_name, bucket_name)
         self.assertEqual(md5_after, md5_before)
 
-        self.logger.info('Stop {} zdb'.format((self.s3.parity)))
+        self.logger.info('stop {} zdb services'.format((self.s3.parity)))
         self.s3.failures.zdb_stop_service(count=self.s3.parity)
 
+        self.logger.info('upload a file, should fail')
+        with self.assertRaises(RuntimeError):
+            self.s3.upload_file()
+
+        self.logger.info('download the old file, should succeed')
         md5_after = self.s3.download_file(file_name, bucket_name)
         self.assertEqual(md5_after, md5_before)
 
-        self.logger.info('Start {} zdb'.format((self.s3.parity)))
-        self.s3.failures.zdb_start_service(count=self.s3.parity)
-
-    def test003_stop_parity_zdb_upload_download_start(self):
-        """
-
-        test003_stop_parity_zdb_upload_download_start
-        - Stop n zdb, n <= parity
-        - upload file, should pass
-        - download file, should pass
-        - assert md5 checksum is matching
-        - start n zdb, should pass
-        """
-        file_name, bucket_name, md5_before = self.s3.upload_file()
-        self.logger.info('stop {} zdb'.format((self.s3.parity)))
-        self.s3.failures.zdb_stop_service(count=self.s3.parity)
-
-        md5_after = self.s3.download_file(file_name, bucket_name)
-        self.assertEqual(md5_after, md5_before)
-
+        self.logger.info('start parity zdb services')
         self.logger.info('start {} zdb'.format((self.s3.parity)))
         self.s3.failures.zdb_start_service(count=self.s3.parity)
 
@@ -70,18 +59,25 @@ class ZDBFailures(BaseTest):
         """
 
         test004_stop_parity_zdb_upload_start_download
-        - Stop n zdb, n <= parity
-        - upload file, should pass
+        - Stop n zdb, n = parity
+        - upload file, should fail
         - start n zdb, should pass
+        - upload file, should succeed
         - download file, should pass
         - assert md5 checksum is matching
         """
-        file_name, bucket_name, md5_before = self.s3.upload_file()
-        self.logger.info('stop {} zdb'.format((self.s3.parity)))
+        self.logger.info('stop {} zdb services'.format((self.s3.parity)))
         self.s3.failures.zdb_stop_service(count=self.s3.parity)
 
-        self.logger.info('start {} zdb'.format((self.s3.parity)))
+        self.logger.info('upload a file, should fail')
+        with self.assertRaises(RuntimeError):
+            self.s3.upload_file()
+
+        self.logger.info('start {} zdb services'.format((self.s3.parity)))
         self.s3.failures.zdb_start_service(count=self.s3.parity)
+
+        self.logger.info('upload file, should pass')
+        file_name, bucket_name, md5_before = self.s3.upload_file()
 
         md5_after = self.s3.download_file(file_name, bucket_name)
         self.assertEqual(md5_after, md5_before)
@@ -96,17 +92,57 @@ class ZDBFailures(BaseTest):
         - Start n+ zdb
         - Download the uploaded file, should succeed
         """
+        self.logger.info('upload file, should pass')
         file_name, bucket_name, md5_before = self.s3.upload_file()
+
         zdb_turn_down = self.s3.parity + randint(1, self.s3.shards)
-        self.logger.info('stop {} zdb'.format(zdb_turn_down))
+        self.logger.info('stop {} zdb services'.format(zdb_turn_down))
         self.s3.failures.zdb_stop_service(count=zdb_turn_down)
 
-        self.logger.info('uploading should fail')
+        self.logger.info('upload a file, should fail')
         with self.assertRaises(RuntimeError):
             self.s3.upload_file()
 
-        self.logger.info('start {} zdb'.format(zdb_turn_down))
+        self.logger.info('start {} zdb services'.format(zdb_turn_down))
         self.s3.failures.zdb_start_service(count=zdb_turn_down)
 
+        md5_after = self.s3.download_file(file_name, bucket_name)
+        self.assertEqual(md5_after, md5_before)
+
+    def test006_upload_stopallzdb_check_start(self):
+        """
+
+        test006_upload_stopallzdb_check_start
+        - Upload file, should succeed
+        - stop all zdbs services
+        - Upload file, should fail
+        - Downalod file, should fail
+        - Start all zdbs
+        - Download old file, should pass
+        - Upload file should pass
+        - Download new file, should succeed
+        """
+        self.logger.info('upload file, should pass')
+        file_name, bucket_name, md5_before = self.s3.upload_file()
+
+        self.namespaces = self.s3.data['namespaces']
+        self.logger.info('stop {} zdb services'.format(len(self.namespaces)))
+        self.s3.failures.zdb_stop_service(count=self.namespaces)
+
+        self.logger.info('uploading should raise an error')
+        with self.assertRaises(RuntimeError):
+            self.s3.upload_file()
+
+        self.logger.info('start {} zdb services'.format(self.namespaces))
+        self.s3.failures.zdb_start_service(count=self.namespaces)
+
+        self.logger.info('download old file, should pass')
+        md5_after = self.s3.download_file(file_name, bucket_name)
+        self.assertEqual(md5_after, md5_before)
+
+        self.logger.info('upload file, should pass')
+        file_name, bucket_name, md5_before = self.s3.upload_file()
+
+        self.logger.info('download new file, should pass')
         md5_after = self.s3.download_file(file_name, bucket_name)
         self.assertEqual(md5_after, md5_before)
