98a6be46dd2cc605d25d4e85066ef747ee25a2f6
0xIslamTaha <islamtaha2012@gmail.com>
2018-12-02 17:42:38 +0200

    add retrying lib

--- test/failures_testsuite/utils/s3.py
+++ test/failures_testsuite/utils/s3.py
@@ -1,5 +1,4 @@
 from jumpscale import j
-import os, time, hashlib
 from zerorobot.service_collection import ServiceNotFoundError
 from gevent.pool import Group
 from utils.reset import EnvironmentReset
@@ -8,11 +7,15 @@ from urllib.parse import urlparse
 from minio import Minio
 from minio.error import BucketAlreadyExists, BucketAlreadyOwnedByYou
 from urllib3.exceptions import ProtocolError
+import os, time, hashlib, tenacity
 
 logger = j.logger.get('s3demo')
 
 
 class S3Manager:
+    retrying = tenacity.Retrying(wait=tenacity.wait_fixed(5),
+                                 retry=tenacity.retry_if_exception_type(IOError))
+
     def __init__(self, parent, name):
         self.failures = FailureGenenator(self)
         self.reset = EnvironmentReset(self)
@@ -88,7 +91,8 @@ class S3Manager:
         zrobot client of the node  that hosts the zos VM which hosts the minio container
         """
         if self._vm_host_robot is None:
-            j.clients.zrobot.get('demo_vm_host_robot', data={'url': "http://%s:6600" % self.vm_host.public_addr}) # 'god_token_': ''
+            j.clients.zrobot.get('demo_vm_host_robot',
+                                 data={'url': "http://%s:6600" % self.vm_host.public_addr})  # 'god_token_': ''
             self._vm_host_robot = j.clients.zrobot.robots['demo_vm_host_robot']
         return self._vm_host_robot
 
@@ -117,7 +121,7 @@ class S3Manager:
 
     @property
     def minio_config(self):
-        return self.minio_container.download_content('/bin/zerostor.yaml')
+        return self.retrying.call(self.minio_container.download_content('/bin/zerostor.yaml'))
 
     @property
     def vm_host(self):
